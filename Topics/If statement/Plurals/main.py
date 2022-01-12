d5c063df77035ea573712e67c6f7dab68587c3e3
Samaa Asif <samaaasif@Samaas-MacBook-Pro.local>
2022-01-12 09:22:33 +0800

    Initial commit

---
+++ Topics/If statement/Plurals/main.py
@@ -0,0 +1,9 @@
+number = int(input())
+word = input()
+
+if number == 0:
+    print(str(number) + " " + word + "s")
+elif number == 1:
+    print(str(number) + " " + word)
+else:
+    print(str(number) + " " + word + "s")
